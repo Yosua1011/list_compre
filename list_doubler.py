694be5683cfe9e5c4e7ed10579073d2d36c021df
Yosua Tresno Surojo <yosua1011@gmail.com>
2017-02-27 14:39:55 +0700

    list_doubler ver1.2

--- list_doubler.py
+++ list_doubler.py
@@ -1,8 +1,7 @@
 my_list = [21, 2, 93]
 
 def list_doubler(lst):
-	doubled = [num * 2 for num in lst]
-	return doubled
+	return [num * 2 for num in lst]
 
 my_doubled_list = list_doubler(my_list)
 print my_doubled_list
